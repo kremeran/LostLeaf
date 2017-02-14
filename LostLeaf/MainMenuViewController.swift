4335aca23448bf3616d6abe496f170ffd9c5ccef
Addison Kremer <addisonkremer@addisons-mini.greek.rose-hulman.edu>
2017-02-14 17:03:01 -0500

    update

--- LostLeaf/MainMenuViewController.swift
+++ LostLeaf/MainMenuViewController.swift
@@ -7,29 +7,23 @@
 //
 
 import UIKit
+import Firebase
 
 class MainMenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pressedLogOut(_ sender: Any) {
+        let firebaseAuth = FIRAuth.auth()
+        do {
+            try firebaseAuth?.signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
